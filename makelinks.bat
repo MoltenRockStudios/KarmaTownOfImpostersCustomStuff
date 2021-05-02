e1fe0e4fc018959e1681871f40a3ef59db4c9c6f
MJDelanty <36679640+MJDelanty@users.noreply.github.com>
2021-05-02 16:59:58 +0100

    Fixing Chris's fuck up a.k.a Mattsuu's

--- makelinks.bat
+++ makelinks.bat
@@ -5,5 +5,5 @@ echo Current Directory: %currentDir%
 
 set /p outputDir=Paste your Among-Us install Directory here: 
 
-MKLINK /D %outputDir%\TownOfImposters_CustomHats %currentDir%\TownOfImposters_CustomHats
-MKLINK /D %outputDir%\TownOfImpostors_Data %currentDir%\TownOfImpostors_Data
+MKLINK /D "%outputDir%\TownOfImpostors_CustomHats" "%currentDir%\TownOfImpostors_CustomHats"
+MKLINK /D "%outputDir%\TownOfImpostors_Data" "%currentDir%\TownOfImpostors_Data"
